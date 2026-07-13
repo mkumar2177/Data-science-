e44e3ca342287057f53423fc3f3e912000428db4
mkumar2177 <mohdkhalidumar1@gmail.com>
2026-07-14 01:09:39 +0530

    linera regreesion add

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+model = pickle.load(open('salary_model.pkl', 'rb'))
+
+st.title("Salary Predictor")
+experience = st.number_input("Enter Years of Experience", min_value=0.0, max_value=50.0, step=0.1)
+
+if st.button("Predict Salary"):
+    prediction = model.predict(np.array([[experience]]))
+    st.success(f"Predicted Salary: ₹{prediction[0]:,.2f}")
