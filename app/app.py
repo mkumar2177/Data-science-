fb0ad08a7c562953855895bbb8a7eb7fb77e957e
mkumar2177 <mohdkhalidumar1@gmail.com>
2026-07-14 13:31:11 +0530

    class 8,app.py

--- app/app.py
+++ app/app.py
@@ -10,3 +10,5 @@ experience = st.number_input("Enter Years of Experience", min_value=0.0, max_val
 if st.button("Predict Salary"):
     prediction = model.predict(np.array([[experience]]))
     st.success(f"Predicted Salary: ₹{prediction[0]:,.2f}")
+
+    
